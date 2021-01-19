69feeb1831416bf8dfc241863ebe23bb16d994e6
gregaswright <greg.a.s.wright@gmail.com>
2021-01-19 10:19:15 -0500

    added new containers and components

---
+++ src/component/SignupForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class LoginFrom extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LoginFrom
---
+++ src/containers/MapContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MapContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    
+}
+
+export default MapContainer
